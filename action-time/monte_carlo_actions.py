9ba78dd3a51241e9579589f839c4e37b34518861
RuthAngus <ruth.angus@astro.ox.ac.uk>
2018-05-14 14:27:46 -0400

    sampling from gaia parameters one star at a time

--- action-time/monte_carlo_actions.py
+++ action-time/monte_carlo_actions.py
@@ -12,34 +12,18 @@ import matplotlib.pyplot as plt
 import pandas as pd
 from astropy.io import fits
 from astropy.table import Table
+import corner
+
+plotpar = {'axes.labelsize':  18,
+           'font.size':       18,
+           'legend.fontsize': 18,
+           'xtick.labelsize': 16,
+           'ytick.labelsize': 16,
+           'text.usetex': True}
+plt.rcParams.update(plotpar)
 
 
-def construct_matrix(means, sigmas, covs):
-    """
-    Construct a covariance matrix.
-    params:
-    ======
-    means: (array)
-        An array containing the value means
-    sigmas: (array)
-        An array containing the variances (diagonal elements).
-    covs: (array)
-        An array containing the covariances between parameters from top left
-        to bottom right.
-    """
-    # Make sure the arrays are all the correct shape.
-    N = len(means)
-    print(N, len(sigmas))
-    print(np.shape(covs), (N-1, N))
-    assert N == len(sigmas)
-    assert np.shape(covs) == (N-1, N)
-
-    C = np.matrix([)
-
-    return cov
-
-
-def gen_sample_set(d, N):
+def gen_sample_set(d, N, parallax=False, plot=False):
     """
     Generate all the samples needed for this analysis by sampling from the
     (Gaussian assumed) likelihood functions.
@@ -50,48 +34,68 @@ def gen_sample_set(d, N):
     N: (int)
         Number of samples.
     """
-    print(d.keys())
 
-    means = np.array([1, 2, 3])
-    sigmas = np.array([.1, .2, .3])
-    aa, bb, cc = .1, .2, .3
-    ab, ac, bc = .4, .5, .6
-    covs_a = np.array([aa, ab, ac])
-    covs_b = np.array([ab, bb, bc])
-    covs_c = np.array([ac, bc, cc])
-    cov = construct_matrix(means, sigmas, covs)
-    assert 0
+    mus = np.array([d.ra.values, d.dec.values, d.pmra.values,
+                    d.pmdec.values])
+    C = np.matrix([
+        [d.ra_error.values**2, d.ra_dec_corr.values,
+            d.ra_pmra_corr.values, d.ra_pmdec_corr.values],
+        [d.ra_dec_corr.values, d.dec_error.values**2,
+            d.dec_pmra_corr.values, d.dec_pmdec_corr.values],
+        [d.ra_pmra_corr.values, d.dec_pmra_corr.values,
+            d.pmra_error.values**2, d.pmra_pmdec_corr.values],
+        [d.ra_pmdec_corr.values, d.dec_pmdec_corr.values,
+            d.pmra_pmdec_corr.values, d.pmdec_error.values**2]
+                    ])
+    labels=["$\mathrm{RA~[degrees]}$", "$\mathrm{dec~[degrees]}$",
+            "$\mu_{\mathrm{RA}}\mathrm{~[mas/year]}$",
+            "$\mu_{\mathrm{dec}}\mathrm{~[mas/year]}$"]
 
-    # assign covariance variables
-    ra_dec = d.tgas_ra_dec_corr.values
-    ra_pmra = d.tgas_ra_pmra_corr.values
-    ra_pmdec = d.tgas_ra_pmdec_corr.values
-    dec_pmra = d.tgas_dec_pmra_corr.values
-    dec_pmdec = d.tgas_dec_pmdec_corr.values
-    pmra_pmdec = d.tgas_pmra_pmdec_corr.values
-
-    mus = np.array([d.ra.values, d.dec.values, d.pmra.values, d.pmdec.values])
-    C = np.matrix([[ra_err**2, ra_dec, ra_pmra, ra_pmdec],
-                   [ra_dec, dec_err**2, dec_pmra, dec_pmdec],
-                   [ra_pmra, dec_pmra, pmra_err**2, pmra_pmdec],
-                   [ra_pmdec, dec_pmdec, pmra_pmdec, pmdec_err**2]
-                   ])
+    if parallax:
+        mus = np.array([d.ra.values, d.dec.values, d.pmra.values,
+                        d.pmdec.values, d.parallax.values])
+        C = np.matrix([
+            [d.ra_error.values**2, d.ra_dec_corr.values,
+                d.ra_pmra_corr.values, d.ra_pmdec_corr.values,
+                d.ra_parallax_corr.values],
+            [d.ra_dec_corr.values, d.dec_error.values**2,
+                d.dec_pmra_corr.values, d.dec_pmdec_corr.values,
+                d.dec_parallax_corr.values],
+            [d.ra_pmra_corr.values, d.dec_pmra_corr.values,
+                d.pmra_error.values**2, d.pmra_pmdec_corr.values,
+                d.parallax_pmra_corr.values],
+            [d.ra_pmdec_corr.values, d.dec_pmdec_corr.values,
+                d.pmra_pmdec_corr.values, d.pmdec_error.values**2,
+                d.parallax_pmdec_corr.values],
+            [d.ra_parallax_corr.values, d.dec_parallax_corr.values,
+                d.parallax_pmra_corr.values,
+                d.parallax_pmdec_corr.values,
+                d.parallax_error.values**2]
+                    ])
+        labels=["$\mathrm{RA~[degrees]}$", "$\mathrm{dec~[degrees]}$",
+                "$\mu_{\mathrm{RA}}\mathrm{~[mas/year]}$",
+                "$\mu_{\mathrm{dec}}\mathrm{~[mas/year]}$",
+                "$\pi\mathrm{~[mas]}$"]
 
     # Sample from the covariance matrix.
     corr_samps = np.random.multivariate_normal(mus, C, size=N).T
-    ra_samps, dec_samps, pmra_samps, pmdec_samps = corr_samps
 
-    # distance_samps
-    # rv_samps
+    if plot:
+        figure = corner.corner(corr_samps.T, labels=labels)
+        if parallax:
+            plt.savefig("covariances_parallax_{}".format(d.kepid.values[i]))
+        else:
+            plt.savefig("covariances_{}".format(d.kepid.values[i]))
 
-    return teff_samps, feh_samps, logg_samps, ra_samps, dec_samps, d_samps, \
-        pmra_samps, pmdec_samps, rv_samps
-
-
-# def sample_from_distance_posterior():
+    return corr_samps
 
 if __name__ == "__main__":
-    dat = Table.read('data/kepler_dr2_4arcsec_updated.fits', format='fits')
-    d = dat.to_pandas()
-    d["relative_parallax_err"] = d.parallax_error.values / d.parallax.values
-    gen_sample_set(d, 10000)
+
+    # Load the csv of the targets, made with the action_time.ipynb notebook.
+    mc = pd.read_csv("data/mcquillan_stars_with_vertical_action.csv")
+    N = 10000
+
+    # Generate samples with covariances for Gaia parameters for each star.
+    nstars = np.shape(mc)[0]
+    for i in range(nstars):
+        samps = gen_sample_set(mc.iloc[i], N)
