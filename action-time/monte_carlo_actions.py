961855d41392e77f6ee9d9e2257bd7d9ab5afaef
RuthAngus <ruth.angus@astro.ox.ac.uk>
2018-05-14 15:17:36 -0400

    adding a function to calculate jz with uncertainties

--- action-time/monte_carlo_actions.py
+++ action-time/monte_carlo_actions.py
@@ -8,11 +8,12 @@ It also uses the Bailer-Jones posterior for distances.
 
 import numpy as np
 import matplotlib.pyplot as plt
-# import paia
 import pandas as pd
 from astropy.io import fits
 from astropy.table import Table
 import corner
+from actions import action
+from tqdm import tqdm
 
 plotpar = {'axes.labelsize':  18,
            'font.size':       18,
@@ -77,7 +78,60 @@ def gen_sample_set(d, N, parallax=False, plot=False):
         else:
             plt.savefig("covariances_{}".format(d.kepid))
 
-    return corr_samps
+    # Sample from distance posterior.
+    d_samps = np.mean([d.r_lo, d.r_hi])*np.random.randn(N) + d.r_est
+    rv_samps = d.radial_velocity_error*np.random.randn(N) + d.radial_velocity
+
+    return corr_samps, d_samps, rv_samps
+
+
+def calc_actions_from_samples(Nsamps, samples, d_samps, rv_samps):
+    """
+    Call Wilma's action function and calculate actions for each sample.
+    params:
+    ======
+    Nsamps: (int)
+        The number of action samples you want. Could be the same as the number
+        of RA, dec, etc samples you generated or could be fewer.
+    samples: (array)
+        2d array of samples for RA, dec, pmra and pmdec. size = (4, Nsamps)
+    d_samps: (array)
+        1d array of distance samples. len = Nsamps.
+    rv_samps: (array)
+        1d array of radial velocity samples. len = Nsamps.
+    return
+    ======
+    action_samps: (2d array)
+        Array containing samples of each of the values calculated by
+        actions.py. These are:
+        R_kpc: Samples of the galactic radius in kpc.
+        phi_rad: Samples of phi_rad FIXME: look up what this is!
+        z_kpc: Samples of the height above the galactic plane.
+        vR_kms: Samples of radial velocity -- check this.
+        vT_kms: Samples of tangential velocity -- check this.
+        vz_kms: Samples of vertical velocity.
+        jR: Samples of Radial action.
+        lz: Samples of ...
+        jz: Samples of vertical action.
+    jz_samps: (1d array)
+        Just the vertical action samples, again.
+    """
+    assert Nsamps <= len(d_samps), "Nsamps must not exceed the number of " \
+        "available samples"
+
+    ra_samps = samples[0, :]
+    dec_samps = samples[1, :]
+    pmra_samps = samples[2, :]
+    pmdec_samps = samples[3, :]
+
+    # R_kpc, phi_rad, z_kpc, vR_kms, vT_kms, vz_kms, jR, lz, jz
+    action_samps = np.zeros((Nsamps, 9))  # actions returns 9 values.
+
+    for i in range(Nsamps):
+        action_samps[i, :] = action(ra_samps[i], dec_samps[i], d_samps[i],
+                                    pmra_samps[i],  pmdec_samps[i],
+                                    rv_samps[i])
+    return action_samps, action_samps[:, -1]
 
 
 if __name__ == "__main__":
@@ -89,4 +143,7 @@ if __name__ == "__main__":
     # Generate samples with covariances for Gaia parameters for each star.
     nstars = np.shape(mc)[0]
     for i in range(nstars):
-        samps = gen_sample_set(mc.iloc[i], N)
+        samps, d_samps, rv_samps = gen_sample_set(mc.iloc[i], N)
+        action_samps, jz_samps = calc_actions_from_samples(10, samps, d_samps,
+                                                           rv_samps)
+        assert 0
